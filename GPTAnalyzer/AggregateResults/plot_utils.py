aac79260bc44829a0dfd27c5749c9a59da26ae4d
Yasaman Razeghi <yrazeghi@uci.edu>
2022-01-23 16:50:18 -0800

    Add frequency finder

--- GPTAnalyzer/AggregateResults/plot_utils.py
+++ GPTAnalyzer/AggregateResults/plot_utils.py
@@ -11,13 +11,19 @@ from typing import List
 TOP_FREQ = 200
 FREQ_NUM = 1
 WORD_FLAG = False
+FREQUENCY_DATA_X_COLUMN = 'testcase.data_point.frequency_data.x'
+FREQUENCY_DATA_Y_COLUMN = 'testcase.data_point.frequency_data.y'
+FREQUENCY_DATA_Z_COLUMN = 'testcase.data_point.frequency_data.z'
 FREQUENCY_DATA_KEY_X_COLUMN = 'testcase.data_point.frequency_data.key'
 FREQUENCY_DATA_KEY_XY_COLUMN = 'testcase.data_point.frequency_data.key_XY'
 FREQUENCY_DATA_KEY_XZ_COLUMN = 'testcase.data_point.frequency_data.key_XZ'
+FREQUENCY_DATA_KEY_YZ_COLUMN = 'testcase.data_point.frequency_data.key_YZ'
 FREQUENCY_X_VALUE_COLUMN = 'testcase.data_point.frequency_data.frequency'
 FREQUENCY_XY_VALUE_COLUMN = 'testcase.data_point.frequency_data.frequency_XY'
 FREQUENCY_XZ_VALUE_COLUMN = 'testcase.data_point.frequency_data.frequency_XZ'
+FREQUENCY_YZ_VALUE_COLUMN = 'testcase.data_point.frequency_data.frequency_YZ'
 IS_CORRECT_COLUMN = 'is_correct'
+NO_WORD_KEYS = ['mult', 'plus', 'concat', 'plushashtag', 'multhashtag', 'compareless', 'comparemore']
 
 
 def log_reg_fit(X, y):
@@ -25,6 +31,53 @@ def log_reg_fit(X, y):
     model = LogisticRegression()
     model.fit(X, y)
     return model.coef_[0], model.intercept_
+
+
+class FrequencyCache:
+    def __init__(self):
+        self.frequency_dicts = {}
+
+    def get_frequency_dict(self, word: str):
+        file_name = "num_2_counting" if (word in NO_WORD_KEYS) else word
+        if file_name in self.frequency_dicts:
+            return self.frequency_dicts[file_name]
+        num2_freq_file_path = f"./num2_counts/{file_name}.txt"
+        frequency_dict = {}
+        with open(num2_freq_file_path) as f:
+            lines = f.readlines()
+            for index, line in enumerate(lines):
+                key, frequency = eval(line)
+                frequency_dict[key] = frequency
+                if (index+1) % 10_000_000 == 0:
+                    print(f"I'm at index {index}, lines: {line}")
+        self.frequency_dicts[file_name] = frequency_dict
+        return self.frequency_dicts[file_name]
+
+
+frequency_cache_singleton = FrequencyCache()
+
+
+def find_pair_frequency(row, first_column: str, second_column: str, word):
+    not_found = 0
+    num_2_frequency = 0
+    min_val = min(str(row[first_column]), str(row[second_column]))
+    max_val = max(str(row[first_column]), str(row[second_column]))
+    if word in NO_WORD_KEYS:
+        tuple1 = (min_val, max_val)
+        tuple2 = (max_val, min_val)
+    else:
+        tuple1 = (min_val, max_val, word)
+        tuple2 = (max_val, min_val, word)
+    freq_dict = frequency_cache_singleton.get_frequency_dict(word)
+    if tuple2 in freq_dict.keys() and not (tuple1 == tuple2):
+            print(f"something is very wrong!!!!!!!!!!! tuple1: {tuple1} tuple2: {tuple2}")
+    if tuple1 in freq_dict.keys():
+        num_2_key = str(tuple1)
+        num_2_frequency = freq_dict[tuple1]
+    else:
+        num_2_key = str(tuple1)
+        not_found = 1
+    return num_2_key, num_2_frequency, not_found
 
 
 class PlotInfo:
@@ -40,6 +93,7 @@ class PlotInfo:
             'x': (FREQUENCY_DATA_KEY_X_COLUMN, FREQUENCY_X_VALUE_COLUMN),
             'xy': (FREQUENCY_DATA_KEY_XY_COLUMN, FREQUENCY_XY_VALUE_COLUMN),
             'xz': (FREQUENCY_DATA_KEY_XZ_COLUMN, FREQUENCY_XZ_VALUE_COLUMN),
+            'yz': (FREQUENCY_DATA_KEY_YZ_COLUMN, FREQUENCY_YZ_VALUE_COLUMN),
         }
         if key_type not in key_value_column_map:
             raise Exception(f"Wrong Key!, key must be one of {key_value_column_map.keys()}")
@@ -73,6 +127,29 @@ class PlotInfo:
         self.data_file.replace(True, 1, inplace=True)
         self.data_file.replace(False, 0, inplace=True)
         self.key_column, self.frequency_value_column = key_value_column_map[key_type]
+        if key_type != 'x':
+            if key_type == 'xy':
+                first_column = FREQUENCY_DATA_X_COLUMN
+                second_column = FREQUENCY_DATA_Y_COLUMN
+            elif key_type == 'xz':
+                first_column = FREQUENCY_DATA_X_COLUMN
+                second_column = FREQUENCY_DATA_Z_COLUMN
+            elif key_type == 'yz':
+                first_column = FREQUENCY_DATA_Y_COLUMN
+                second_column = FREQUENCY_DATA_Z_COLUMN
+            else:
+                raise Exception("Should not be here!")
+            total_not_found = 0
+            for i, row in self.data_file.iterrows():
+                new_key, new_frequency, not_found = find_pair_frequency(row=row,
+                                                                               first_column=first_column,
+                                                                               second_column=second_column,
+                                                                               word=self.word)
+                self.data_file.at[i, self.key_column] = new_key
+                self.data_file.at[i, self.frequency_value_column] = new_frequency
+                total_not_found += not_found
+            if total_not_found > 0:
+                print(f"Total Not Found for key {key_type} is {total_not_found}")
         self.aggregated_data_by_key = self.data_file.groupby(self.key_column)[
             [IS_CORRECT_COLUMN, self.frequency_value_column]].mean()
 
@@ -113,7 +190,7 @@ class PlotInfo:
     def calculate_logistic_regression(self):
         freq_all = self.data_file[self.frequency_value_column].to_numpy()
         accuracy_all = self.data_file[IS_CORRECT_COLUMN].to_numpy()
-        frequencies_log10 = np.log10(freq_all)
+        frequencies_log10 = np.ma.log10(freq_all)
         coef, intercept = log_reg_fit(frequencies_log10, accuracy_all)
         self.coef = coef
         self.intercept = intercept
@@ -162,11 +239,11 @@ class PlotInfo:
         return model_name_map[self.model_key]
 
 
-def save_freq_acc_plot_and_get_info(word: str, shots: int, model: str, key: str, show_plot: bool = False):
+def save_freq_acc_plot_and_get_info(word: str, shots: int, model: str, key: str, show_plot: bool = False, quantile_number: int = 10):
     # Creat PlotInfo
     plot_info = PlotInfo(word, shots, model, key_type=key)
     plot_info.calculate_spearman()
-    plot_info.quantile_accuracies_plot(q_num=10)
+    plot_info.quantile_accuracies_plot(q_num=quantile_number)
     plot_info.calculate_logistic_regression()
     plot_info.calculate_accuracy_all()
 
@@ -223,10 +300,10 @@ def save_logistic_regression_lines_plot_for_shots(word: str, model: str, shots: 
         plot_info = PlotInfo(word, shot, model, key_type= key)
         mode = plot_info.get_mode()
         model_name = plot_info.get_model()
-        plot_info.calculate_spearman()
-        plot_info.quantile_accuracies_plot(q_num=10)
+        # plot_info.calculate_spearman()
+        # plot_info.quantile_accuracies_plot(q_num=10)
         plot_info.calculate_logistic_regression()
-        plot_info.calculate_accuracy_all()
+        # plot_info.calculate_accuracy_all()
 
         t = plot_info.logistic_regression_t
         p_t = plot_info.logistic_regression_pt
@@ -249,10 +326,10 @@ def save_logistic_regression_lines_plot_for_models(word: str, models: List[str],
         # Creat PlotInfo
         plot_info = PlotInfo(word, shot, model, key_type=key)
         mode = plot_info.get_mode()
-        plot_info.calculate_spearman()
-        plot_info.quantile_accuracies_plot(q_num=10)
+        # plot_info.calculate_spearman()
+        # plot_info.quantile_accuracies_plot(q_num=10)
         plot_info.calculate_logistic_regression()
-        plot_info.calculate_accuracy_all()
+        # plot_info.calculate_accuracy_all()
 
         t = plot_info.logistic_regression_t
         p_t = plot_info.logistic_regression_pt
